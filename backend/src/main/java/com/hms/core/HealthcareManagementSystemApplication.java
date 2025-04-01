20589f7a157356923a5097b531484cf91d843fc9
Supraj Bachawala <bachawalasupraj@gmail.com>
2025-03-31 23:38:17 -0400

    init spring boot

---
+++ backend/src/main/java/com/hms/core/HealthcareManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hms.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareManagementSystemApplication.class, args);
+	}
+
+}
